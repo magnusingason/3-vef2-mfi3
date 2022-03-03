e940bb0152c5a9e5a79aa1f097ca9efc665a5b29
magnusingason <80974014+magnusingason@users.noreply.github.com>
2022-03-03 21:24:27 +0000

    Update login.js

--- src/lib/login.js
+++ src/lib/login.js
@@ -61,6 +61,7 @@ export function ensureLoggedIn(req, res, next) {
 }
 
 export function requireAuthentication(req, res, next) {
+  console.log("i get here");
   return passport.authenticate(
     'jwt',
     { session: false },
