d796ebf1c27f66718557e1f31d1cfa21d7e3d006
magnusingason <80974014+magnusingason@users.noreply.github.com>
2022-03-04 15:01:24 +0000

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -129,7 +129,7 @@ export async function listEvents() {
   const result = await query(q);
 
   if (result) {
-    console.log(results.rows);
+    console.log(result.rows);
     return result.rows;
   }
 
