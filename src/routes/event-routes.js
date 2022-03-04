2b35bc561bfdb6b1388561059a6e64cd95379de9
magnusingason <80974014+magnusingason@users.noreply.github.com>
2022-03-04 18:45:01 +0000

    Update event-routes.js

--- src/routes/event-routes.js
+++ src/routes/event-routes.js
@@ -60,7 +60,7 @@ eventsRouter.delete('/:id', requireAuthentication, async (req, res) => {
 eventsRouter.post('/:id/register', requireAuthentication, async (req, res) => {
     const { id: idEvent } = req.params.id;
     const id = req.user;
-    console.log(idEvent);
+    console.log(id);
     const event = await getEventById(idEvent);
     const user = findById(id);
     const name = user.name;
