f97a8b7b55db429713024ffcdbcb62024019d401
magnusingason <80974014+magnusingason@users.noreply.github.com>
2022-03-04 16:17:26 +0000

    Update event-routes.js

--- src/routes/event-routes.js
+++ src/routes/event-routes.js
@@ -13,6 +13,7 @@ eventsRouter.get('/', async (req, res) => {
 eventsRouter.post('/', requireAuthentication, async (req, res) => {
     const { name, description = '' } = req.body;
     const slug = slugify(name);
+    console.log(name);
     const created = await createEvent({ name, slug, description });
     return res.json({ created });
 })
